aaa7f1f3bdca397561488090e4c6a429026bfa83
mehdi-ceppic <formker@gmail.com>
2025-10-21 11:19:12 +0200

    Ajout liste des cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import userData from "./assets/datausers.json";
 import './App.css'
 
 function App() {
+  const allUsers = userData.results;
 
   return (
     <>
       <h1>Randomusers</h1>
+      <section className="userCards">
+        {allUsers.map((user) => (
+          <div
+            key={user.login.uuid}
+            className="userCard"
+          >
+            <img src={user.picture.large} alt="" />
+            <h3>
+              {user.name.first} {user.name.last}
+            </h3>
+            <p>{user.location.city}</p>
+            <p>{user.dob.age} ans</p>
+          </div>
+        ))}
+      </section>
     </>
   )
 }
