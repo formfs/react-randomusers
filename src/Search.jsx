0972af228cb0a0c076459c70c4b46f68c9e75c87
mehdi-ceppic <formker@gmail.com>
2025-10-21 14:13:23 +0200

    Ajout filtre de recherche

--- src/Search.jsx
+++ src/Search.jsx
@@ -3,21 +3,33 @@ import userData from "./assets/datausers.json";
 import "./Search.css";
 
 function Search() {
-  const [search, setSearch] = useState("");
   const allUsers = userData.results;
+  const [search, setSearch] = useState("");
+
+  const filtered = allUsers.filter(
+    (user) => {
+        const name = `${user.name.first} ${user.name.last}`.toLowerCase();
+        const city = user.location.city.toLowerCase();
+        const age = user.dob.age.toString();
+        return (
+            name.includes(search.toLowerCase()) || city.includes(search.toLowerCase()) || age.includes(search.toString())
+        )
+        ;
+    });
 
   return (
     <>
       <h1>Randomusers</h1>
       <div className="searchInput">
-        <input 
-        type="text" 
-        placeholder="Search ..." 
-        value={search}
+        <input
+          type="text"
+          placeholder="Search ..."
+          value={search}
+          onChange={(valeurInput) => setSearch(valeurInput.target.value)}
         />
       </div>
       <section className="userCards">
-        {allUsers.map((user) => (
+        {filtered.map((user) => (
           <div key={user.login.uuid} className="userCard">
             <img src={user.picture.large} alt="" />
             <h3>
